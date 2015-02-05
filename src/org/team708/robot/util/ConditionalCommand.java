9353f6c3823e93490b30b42d1ce53ccce0ffa558
Nam Tran <tranngocnam97@yahoo.com>
2015-02-04 20:32:02 -0500

    Added empty new command classes

---
+++ src/org/team708/robot/util/ConditionalCommand.java
@@ -0,0 +1,56 @@
+package org.team708.robot.util;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ConditionalCommand extends Command {
+
+	public ConditionalCommand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConditionalCommand(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConditionalCommand(double timeout) {
+		super(timeout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConditionalCommand(String name, double timeout) {
+		super(name, timeout);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/team708/robot/util/PublicCommand.java
@@ -0,0 +1,56 @@
+package org.team708.robot.util;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class PublicCommand extends Command {
+
+	public PublicCommand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PublicCommand(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PublicCommand(double timeout) {
+		super(timeout);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PublicCommand(String name, double timeout) {
+		super(name, timeout);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
