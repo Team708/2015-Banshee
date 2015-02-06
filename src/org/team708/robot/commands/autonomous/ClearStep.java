ba34778c75fc0898f0d3a1269e9d004db0c826b4
katzekitteh <cowanwalls@gmail.com>
2015-02-06 16:41:36 -0500

    Hotfixed the drivetrain not doing anything when it should
    Temporary commit for master hotfix

--- src/org/team708/robot/commands/autonomous/ClearStep.java
+++ src/org/team708/robot/commands/autonomous/ClearStep.java
@@ -1,36 +1,28 @@
 package org.team708.robot.commands.autonomous;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
-public class ClearStep extends Command {
+public class ClearStep extends CommandGroup {
+    
+    public  ClearStep() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
 
-    public ClearStep() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
     }
 }
