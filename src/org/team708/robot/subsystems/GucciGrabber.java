6584248d669f0dc89777619069304b4e11ce1d2d
omn0mn0m <tranngocnam97@yahoo.com>
2015-03-29 11:18:50 -0400

    Started the gucci grabber

---
+++ src/org/team708/robot/subsystems/GucciGrabber.java
@@ -0,0 +1,17 @@
+package org.team708.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class GucciGrabber extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
