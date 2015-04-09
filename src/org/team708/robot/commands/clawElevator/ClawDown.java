ee9eb3bb9a66c893bace16092135eebd9db63258
omn0mn0m <tranngocnam97@yahoo.com>
2015-04-09 09:01:47 -0400

    Adjusted the cancel logic for the move commands

--- src/org/team708/robot/commands/clawElevator/ClawDown.java
+++ src/org/team708/robot/commands/clawElevator/ClawDown.java
@@ -32,9 +32,9 @@ public class ClawDown extends Command {
     	
     	if (isAtLimit) {
     		cancel();
+    	} else {
+    		Robot.clawElevator.resetEncoder();
     	}
-    	
-    	Robot.clawElevator.resetEncoder();
     	
 //    	if (Robot.clawElevator.getContainerHeight() == 0) {
     		Robot.clawElevator.decrementContainerHeight();
