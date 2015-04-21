4711076fdde15ba1e027d73fcef0195a03f962ab
omn0mn0m <tranngocnam97@yahoo.com>
2015-04-21 18:01:07 -0400

    Adjusted auto for the can burglars

--- src/org/team708/robot/commands/autonomous/encoder/CanGrabMoveBack.java
+++ src/org/team708/robot/commands/autonomous/encoder/CanGrabMoveBack.java
@@ -6,6 +6,7 @@ import org.team708.robot.commands.drivetrain.DriveStraightToEncoderDistance;
 import org.team708.robot.commands.gucciGrabber.ActuateGucciGrabber;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -17,6 +18,7 @@ public class CanGrabMoveBack extends CommandGroup {
     	
         addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, 0.65, false));
         addSequential(new ActuateGucciGrabber(true));
+        addSequential(new WaitCommand(0.1));
         
         addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, 0.65));
     }
--- src/org/team708/robot/commands/autonomous/encoder/CanGrabMoveToNext.java
+++ src/org/team708/robot/commands/autonomous/encoder/CanGrabMoveToNext.java
@@ -7,6 +7,7 @@ import org.team708.robot.commands.drivetrain.TurnToDegrees;
 import org.team708.robot.commands.gucciGrabber.ActuateGucciGrabber;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -18,13 +19,17 @@ public class CanGrabMoveToNext extends CommandGroup {
     	
         addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false));
         addSequential(new ActuateGucciGrabber(true));
+        addSequential(new WaitCommand(0.1));
         
         addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED));
+        addSequential(new ActuateGucciGrabber(false));
         addSequential(new TurnToDegrees(AutoConstants.TURN_SPEED, AutoConstants.NINETY_DEGREE_TURN));
         addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_SECOND_CAN_DISTANCE, AutoConstants.ENCODER_SPEED));
         
         addSequential(new TurnToDegrees(AutoConstants.TURN_SPEED, -AutoConstants.NINETY_DEGREE_TURN));
+        addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false));
         addParallel(new ActuateGucciGrabber(true));
+        addSequential(new WaitCommand(0.1));
         addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false));
     }
 }
