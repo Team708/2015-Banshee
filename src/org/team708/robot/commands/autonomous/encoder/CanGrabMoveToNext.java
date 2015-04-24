47a58db26a4aac306390aeb8eb01110009fb44a6
omn0mn0m <tranngocnam97@yahoo.com>
2015-04-24 09:12:23 -0500

    Added a time failsafe to the can grab moving

--- src/org/team708/robot/commands/autonomous/encoder/CanGrabMoveToNext.java
+++ src/org/team708/robot/commands/autonomous/encoder/CanGrabMoveToNext.java
@@ -3,6 +3,7 @@ package org.team708.robot.commands.autonomous.encoder;
 import org.team708.robot.AutoConstants;
 import org.team708.robot.commands.claw.CloseClaw;
 import org.team708.robot.commands.drivetrain.DriveStraightToEncoderDistance;
+import org.team708.robot.commands.drivetrain.DriveStraightToEncoderDistanceOrTime;
 import org.team708.robot.commands.drivetrain.TurnToDegrees;
 import org.team708.robot.commands.gucciGrabber.ActuateGucciGrabber;
 
@@ -17,7 +18,7 @@ public class CanGrabMoveToNext extends CommandGroup {
     public  CanGrabMoveToNext() {
     	addParallel(new CloseClaw());
     	
-        addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false));
+        addSequential(new DriveStraightToEncoderDistanceOrTime(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false, AutoConstants.CAN_GRAB_TIMEOUT));
         addSequential(new ActuateGucciGrabber(true));
         addSequential(new WaitCommand(0.1));
         
@@ -27,7 +28,7 @@ public class CanGrabMoveToNext extends CommandGroup {
         addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_SECOND_CAN_DISTANCE, AutoConstants.ENCODER_SPEED));
         
         addSequential(new TurnToDegrees(AutoConstants.TURN_SPEED, -AutoConstants.NINETY_DEGREE_TURN));
-        addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false));
+        addSequential(new DriveStraightToEncoderDistanceOrTime(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false, AutoConstants.CAN_GRAB_TIMEOUT));
         addParallel(new ActuateGucciGrabber(true));
         addSequential(new WaitCommand(0.1));
         addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false));
--- src/org/team708/robot/commands/autonomous/encoder/CanGrabNoMove.java
+++ src/org/team708/robot/commands/autonomous/encoder/CanGrabNoMove.java
@@ -2,7 +2,7 @@ package org.team708.robot.commands.autonomous.encoder;
 
 import org.team708.robot.AutoConstants;
 import org.team708.robot.commands.claw.CloseClaw;
-import org.team708.robot.commands.drivetrain.DriveStraightToEncoderDistance;
+import org.team708.robot.commands.drivetrain.DriveStraightToEncoderDistanceOrTime;
 import org.team708.robot.commands.gucciGrabber.ActuateGucciGrabber;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -15,7 +15,7 @@ public class CanGrabNoMove extends CommandGroup {
     public  CanGrabNoMove() {
     	addParallel(new CloseClaw());
     	
-        addSequential(new DriveStraightToEncoderDistance(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false));
+        addSequential(new DriveStraightToEncoderDistanceOrTime(AutoConstants.TO_CAN_DISTANCE, AutoConstants.ENCODER_SPEED, false, AutoConstants.CAN_GRAB_TIMEOUT));
         addSequential(new ActuateGucciGrabber(true));
     }
 }
